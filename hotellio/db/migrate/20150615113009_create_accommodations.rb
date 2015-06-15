661374bd8ed6398dc31209e7559529f7f383ebf7
Vadim Popowsky <vadimpopowsky@gmail.com>
2015-06-15 13:46:59 +0200

    models and controllers

---
+++ hotellio/db/migrate/20150615113009_create_accommodations.rb
@@ -0,0 +1,13 @@
+class CreateAccommodations < ActiveRecord::Migration
+  def change
+    create_table :accommodations do |t|
+      t.string :title
+      t.text :description
+      t.integer :price
+      t.string :type
+      t.integer :guest_number
+
+      t.timestamps null: false
+    end
+  end
+end
