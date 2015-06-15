341eb03127c2c3e94ce342fc2ef137e63015e700
AndFabi <fabiandreacola@gmail.com>
2015-06-15 16:38:29 +0200

    fake-room

--- hotellio/db/seeds.rb
+++ hotellio/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Accommodation.create(title: "italy", description: "jesus born", price: "220", type: "entire flat/house", guest_number:"3")
+Accommodation.create(title: "london", description: "beautifull view", price: "500", type: "private room", guest_number:"5")
+Accommodation.create(title: "new-york", description: "beautifull place", price: "215", type: "private room", guest_number:"6")
+Accommodation.create(title: "brussels", description: "Place to be", price: "50", type: "shared room", guest_number:"7")
+Accommodation.create(title: "japan", description: "i don't know", price: "154", type: "private room", guest_number:"1")
+Accommodation.create(title: "Vietnam", description: "zsvedgsdfwgqbv", price: "458", type: "shared room", guest_number:"12")
+Accommodation.create(title: "Paris", description: "Finally", price: "12", type: "private room", guest_number:"6")
