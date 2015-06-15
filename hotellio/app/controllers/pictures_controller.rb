e82a9907cd02555789b1501dca386e932b4062c5
Marc Debnicki <you@example.com>
2015-06-15 15:34:44 +0200

    Room pic + Review controllers done

--- hotellio/app/controllers/pictures_controller.rb
+++ hotellio/app/controllers/pictures_controller.rb
@@ -1,13 +1,38 @@
 class PicturesController < ApplicationController
+
+  before_action :find_accommodation, only: [:create, :destroy]
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
   def new
+    @picture = Picture.new
   end
 
   def create
+    @picture = @accommodation.pictures.build(picture_params)
+    if @picture.save
+      redirect_to accommodation_show_path(@accommodation)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to accommodation_show_path(@accommodation)
   end
 
-  def show
+  private
+
+  def picture_params
+    params.require(:picture).permit(:caption)
   end
+
+  def find_accommodation
+     @accommodation = Accommodation.find(params[:accommodation_id])
+  end
+
 end
--- hotellio/app/controllers/reviews_controller.rb
+++ hotellio/app/controllers/reviews_controller.rb
@@ -1,13 +1,34 @@
 class ReviewsController < ApplicationController
-  def new
-  end
 
-  def create
+  before_action :find_accommodation, only: [:create]
+
+  def index
+    @reviews = Review.all
   end
 
   def show
   end
 
-  def index
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = @accommodation.reviews.build(review_params)
+    if @review.save
+      redirect_to accommodation_show_path(@review)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :description)
+  end
+
+  def find_accommodation
+    @accommodation = Accommodation.find(params[:accommodation_id])
   end
 end
