44ee8c222be6366c2ce4d5ae63198416f6175282
Vadim Popowsky <vadimpopowsky@gmail.com>
2015-06-16 14:34:32 +0200

    adapted

--- hotellio/app/models/picture.rb
+++ hotellio/app/models/picture.rb
@@ -1,10 +1,8 @@
 class Picture < ActiveRecord::Base
   belongs_to :accommodation
 
-  validates :caption, presence: true
-
   has_attached_file :file,
-    style: { medium: "300x300>", thumb: "100x100>" }
+    styles: { medium: "300x300>", thumb: "100x100>" }
 
   validates_attachment_content_type :file,
     content_type: /\Aimage\/.*\z/
