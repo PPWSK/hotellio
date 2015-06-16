157daf5090624aad48a69a9fe39ab762fa805263
Vadim Popowsky <vadimpopowsky@gmail.com>
2015-06-16 15:57:00 +0200

    start of a search form

--- hotellio/app/models/search.rb
+++ hotellio/app/models/search.rb
@@ -1,9 +1,9 @@
 class Search
   include ActiveModel::Model
-  include ActiveModel::Validations
+
 
   attr_accessor :location, :start_date, :end_date, :number_rooms
 
-  validates :location, presence: true
+  validates :number_rooms, presence: true
 
 end
