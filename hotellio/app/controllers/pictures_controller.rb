1a47420a885845a45cf453f35b99b23659110859
Vadim Popowsky <vadimpopowsky@gmail.com>
2015-06-18 14:08:20 +0200

    routes redirected and able to add, remove picture in edit accommodation

--- hotellio/app/controllers/pictures_controller.rb
+++ hotellio/app/controllers/pictures_controller.rb
@@ -13,7 +13,9 @@ class PicturesController < ApplicationController
   def create
     @picture = @accommodation.pictures.build(picture_params)
     if @picture.save
-      redirect_to accommodation_path(@accommodation)
+      redirect_to edit_user_accommodation_path(
+        params[:user_id],
+        params[:accommodation_id])
     else
       render :new
     end
@@ -21,8 +23,11 @@ class PicturesController < ApplicationController
 
   def destroy
     @picture = Picture.find(params[:id])
-    @picture.destroy
-    redirect_to accommodation_path(@accommodation)
+    if @picture.destroy
+      redirect_to edit_user_accommodation_path(
+        params[:user_id],
+        params[:accommodation_id])
+    end
   end
 
   private
