13003e3d1d2df89aaddc77f7f1ae26b6a9a6faae
AndFabi <fabiandreacola@gmail.com>
2015-06-18 15:06:45 +0200

    searchquery matches with criteria

--- hotellio/app/models/accommodation.rb
+++ hotellio/app/models/accommodation.rb
@@ -15,42 +15,31 @@ class Accommodation < ActiveRecord::Base
   def self.search(search_query)
 
     # only check guest number if filled in
-    results = Accommodation.all
 
-    if search_query.location.present?
-      results = Accommodation.near(search_query.location, 20)
-      if results.size.zero?
-        # flash[:notice] = "No result found"
-        # @accommodations = Accommodation.all
-      end
+    #results = Accommodation.near(search_query.location, 20).where(guest_number: search_query.number_rooms)
+
+    if search_query.location.present? && search_query.number_rooms.present? #&& dates
+
+      Accommodation.near(search_query.location, 20).where(guest_number: search_query.number_rooms)
+
+    elsif search_query.location.present? && search_query.number_rooms.present?
+
+        Accommodation.near(search_query.location, 20).where(guest_number: search_query.number_rooms)
+
+    elsif search_query.number_rooms.present?
+
+        Accommodation.where(guest_number: search_query.number_rooms)
+
+    elsif search_query.location.present?
+
+        Accommodation.near(search_query.location, 20)
+
+    #elsif search_query.dates
+
+    else
+      Accommodation.all
+
     end
 
-    # if search_query.number_rooms.present?
-    #   results = results.select do |result|
-    #     result.guest_number == search_query.number_rooms
-    #   end
-    # end
-
-    # if (search_query.start_date.present? && search_query.end_date.present?)
-    #   results = results.select do |accommodation|
-    #     accommodation.start_date <= search_query.start_date && accommodation.end_date >= search_query.end_date
-    #   end
-    # end
-
-    return results
-
-    # where(guest_number: search_query.number_rooms)
-
-    # LIke for sqlite, ILike for postgres
-
-    # Flat.near([40.71, 100.23], 20)    # venues within 10 km of a point
-    # where("title like ?", "%#{query}%")
-
-    # User.where({ created_at: (Time.now.midnight - 1.day)..Time.now.midnight })
-    # SELECT * FROM users WHERE (created_at BETWEEN '2012-06-09 07:00:00.000000' AND '2012-06-10 07:00:00.000000')
-
-    # nearbys method
-    # The nearbys method must be applied on an object not on a Class. It allows you to find other objects near the given one specifying the distance like the example below :
-    # @nearby_classifieds = @classified.nearbys(10) # 10 is the distance parameter (in kilometers in our case)
   end
 end
