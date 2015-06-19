fc50acc5e89cd3897f180733e0ae895d4eef9fd9
Anna Lilja Lydsdottir <annaliljal@gmail.com>
2015-06-19 13:33:42 +0200

    user profile, my_profile and a stylesheet. needs testing (missing paths from new master)

--- hotellio/app/models/user.rb
+++ hotellio/app/models/user.rb
@@ -4,7 +4,12 @@ class User < ActiveRecord::Base
   has_many :bookings
   has_one :account
 
+  has_attached_file :picture,
+   styles: { medium: "300x300>", small: "50x50>", thumb: "100x100>" }
+
   validates :first_name, :last_name, :birthdate, :gender, :description, :phone_number, presence: true
   validates :phone_number, uniqueness: true
   validates :gender, inclusion: { in: ["female", "male", "other"] }
+  validates_attachment_content_type :picture,
+   content_type: /\Aimage\/.*\z/
 end
